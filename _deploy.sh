0b6cbce8baa4e3406bfddb243ec75889f8608f51
Jason Weathered <git@jason.backtick.org>
2010-01-03 19:17:56 +1000

    Update deploy script to send email on failures since GitHub discards any output.

--- _deploy.sh
+++ _deploy.sh
@@ -3,12 +3,24 @@ if [ ! -d .git ]
 then
 	echo Content-Type: text/plain
 	echo
-	cd ../repo
-	git fetch origin
-	git reset -q --hard origin/master
-	git clean -qxf
-	chmod +x _deploy.sh
-	./_deploy.sh
+	LOG_FILE="`mktemp -t jasoncodes_deploy.log.XXXXXX`"
+	trap '{ rm -f "$LOG_FILE"; }' EXIT
+	(
+		set -e
+		cd ../repo
+		git fetch origin
+		git reset -q --hard origin/master
+		git clean -qxf
+		chmod +x _deploy.sh
+		./_deploy.sh
+	) > "$LOG_FILE" 2>&1
+	if [ "$?" -eq 0 ]
+	then
+		echo Deploy succeeded.
+	else
+		echo Deploy failed.
+		mail -s "jasoncodes.com deploy error" "`whoami`" < "$LOG_FILE"
+	fi
 else
 	cd ..
 	[ -e public_html.tmp ] && rm -rf public_html.tmp
