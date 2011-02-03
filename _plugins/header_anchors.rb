26b8bf4c5744cf6afdf6ce0ac0732c49fc477a38
Jason Weathered <jason@jasoncodes.com>
2011-02-03 17:26:59 +1000

    Support header anchors in Markdown with custom syntax.
    Converts `## Foo Bar [example]` to `<h2 id="example">Foo Bar</h2>`.
    
    This allows one to specify precise anchor names for linking to sections. `generate_toc` is nice but links break if you change the wording.

---
+++ _plugins/header_anchors.rb
@@ -0,0 +1,13 @@
+require 'active_support'
+require 'active_support/core_ext/module/aliasing'
+
+# convert `## Foo Bar [example]` to `<h2 id="example">Foo Bar</h2>`
+
+class Jekyll::MarkdownConverter
+  def convert_with_header_anchors(content)
+    html = convert_without_header_anchors(content)
+    html.gsub! %r[^<(h\d+)[^>]*>(.*) \[([^\]]+)\]</\1>], '<\1 id="\3">\2</\1>'
+    html
+  end
+  alias_method_chain :convert, :header_anchors
+end
